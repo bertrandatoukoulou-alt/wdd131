446d3e7bd07161a68d20853fe6b6de2489a0fae3
Bertrand Atoukoulou <katoukoulou@byupathway.edu>
2026-07-08 18:12:32 +0000

    first

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Insert current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Insert last modified date of the page
+document.getElementById("lastModified").textContent = document.lastModified;
